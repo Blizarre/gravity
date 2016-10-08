fd125be324ed2c0d79d274e522339c1ed7dda369
Simon M <simon.marache@gmail.com>
2016-10-08 13:01:02 +0000

    first version with gui, physics still not perfect

--- src/phys.ts
+++ src/phys.ts
@@ -1,5 +1,16 @@
+let WIDTH = 1024;
+let HEIGHT = 768;
+
+const NB_PLANETS = 2;
+//const G_CST = 6.67e-11 //  N (Mm/kg)2;
+//const M = 5.972e24; // earth mass
+
+const G_CST = 6.67e-11 //  N (Mm/kg)2;
+const M = 5.972e14; // a mass
+
+
 class Point {
-    constructor(public position: Vector, public speed: Vector = new Vector(0, 0)) { }
+    constructor(public position: Vector, public velocity: Vector = new Vector(0, 0)) { }
     static new(position: Vector) { return new Point(position, new Vector(0, 0)); }
 }
 
@@ -14,6 +25,9 @@ class Vector {
         return this;
     }
 
+    equal(other:Vector): boolean {
+        return this.x == other.x && this.y == other.y;
+    }
 
     sub(other: Vector): Vector {
         return new Vector(this.x - other.x, this.y - other.y);
@@ -23,7 +37,10 @@ class Vector {
         this.y -= other.y;
         return this;
     }
-    
+
+    div(other: number): Vector {
+        return new Vector(this.x / other, this.y / other);
+    }
     divIp(other: number): Vector {
         this.x /= other;
         this.y /= other;
@@ -33,7 +50,7 @@ class Vector {
     norm(): number {
         return Math.sqrt(this.normSquare());
     }
-    
+
     normSquare(): number {
         return this.x * this.x + this.y * this.y;
     }
@@ -48,35 +65,96 @@ class Vector {
     }
 }
 
-const NB_ELEMENTS = 100;
-const G_CST = 1000;
-const M = 1;
 
-// The workd starts with NB_ELEMENTS points in the plane [0, 1[, [0, 1[ 
-let world: Point[] = [];
-for (let i = 0; i < NB_ELEMENTS; i++) {
-    world.push(Point.new(new Vector(Math.random(), Math.random())));
-}
+class World {
 
-function update(world: Point[]) {
-    // Update the speed vector
-    for (let p1 of world) {
-        let average_pull = new Vector(0, 0);
-        for (let p2 of world) {
-            // pull_vector = direction_vector * m1*m2*G / (distance * distance)
-            
-            // this is direction_vector * distance
-            let pull_vector: Vector = p2.position.sub(p1.position);
-            
-            let distance: number = pull_vector.norm();
-            // to get X / distance^2, need to divide byt distance ^ 3
-            pull_vector.divIp( distance * distance * distance ).mulIp( G_CST * M * M);
-            average_pull.add(pull_vector);
+    planets: Point[] = [];
+
+    constructor(public width: number, public height:number, public nb_planets: number) {
+        for (let i = 0; i < nb_planets; i++) {
+            this.planets.push(Point.new(new Vector(Math.random() * this.width, Math.random() * this.height)));
         }
-        p1.speed.add(average_pull);
     }
-    // update the position vector
-    for (let p of world) {
-        p.position.add(p.speed);   
+
+    compute_force(planet: Point, other:Point): Vector {
+        // this is direction_vector * distance
+        let diff: Vector = other.position.sub(planet.position);
+
+        let distance: number = diff.norm();
+        // to get X / distance^2, need to divide byt distance ^ 3
+        return diff.divIp( distance * distance * distance ).mulIp( G_CST * M * M);
+    }
+
+    update() {
+        // update the position vector
+        for (let p of this.planets) {
+            p.position.addIp(p.velocity);
+        }
+
+        // Update the velocity vector
+        for (let p1 of this.planets) {
+            let sum_pull = new Vector(0, 0);
+            for (let p2 of this.planets) {
+                // use the velocity Verlet method instead: http://gamedev.stackexchange.com/questions/15708/how-can-i-implement-gravity
+                if(! p1.position.equal(p2.position)) {
+                    // pull_vector = direction_vector * m1*m2*G / (distance * distance)
+                    sum_pull.addIp(this.compute_force(p1, p2));
+                }
+            }
+            let acceleration = sum_pull.div(M);
+            p1.velocity.addIp(acceleration);
+        }
     }
 }
+
+
+class DrawingBoard {
+    constructor(public world: World, public ctx: CanvasRenderingContext2D) {}
+
+    drawPoint(imageData: ImageData, location: Vector) {
+        if( location.y >= 0 && location.y < imageData.height && location.x >= 0 && location.x < imageData.width) {
+            let offset: number = (location.y | 0) * (imageData.width*4) + (location.x | 0) * 4;
+            imageData.data[offset + 0] = 255;
+            imageData.data[offset + 1] = 255;
+            imageData.data[offset + 2] = 255;
+            imageData.data[offset + 3] = 255;
+        }
+    }
+
+    draw() {
+        ctx.fillStyle = "black";
+        ctx.fillRect(0, 0, ctx.canvas.clientWidth, ctx.canvas.clientHeight);
+
+        let imageData = ctx.getImageData(0, 0, ctx.canvas.clientWidth, ctx.canvas.clientHeight);
+
+        for (let p of world.planets) {
+            this.drawPoint( imageData, new Vector(p.position.x, p.position.y) );
+        }
+
+        ctx.putImageData(imageData, 0, 0);
+    }
+}
+
+
+
+let $canv = document.createElement("canvas");
+let $message = document.createElement("p");
+
+$canv.width = WIDTH;
+$canv.height = HEIGHT;
+
+document.body.appendChild($canv);
+document.body.appendChild($message);
+
+let ctx: CanvasRenderingContext2D = $canv.getContext("2d");
+
+let world = new World(WIDTH, HEIGHT, NB_PLANETS);
+let drawingBoard = new DrawingBoard(world, ctx);
+
+let counter = 0
+setInterval( () => {
+    world.update();
+    drawingBoard.draw();
+    $message.innerHTML = "Frame: " + counter;
+    counter ++;
+}, 500);
