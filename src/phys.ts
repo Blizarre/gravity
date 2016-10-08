457137f69a2b6ba4a4554a16bd315f507e74dade
Simon M <simon.marache@gmail.com>
2016-10-08 22:43:30 +0000

    upgrade algorithm from euler to velvet integration

--- src/phys.ts
+++ src/phys.ts
@@ -1,7 +1,7 @@
-let WIDTH = 1024;
-let HEIGHT = 768;
-
-const NB_PLANETS = 2;
+let WIDTH = 640;
+let HEIGHT = 480;
+const TIMESTEP = 0.5
+const NB_PLANETS = 10;
 //const G_CST = 6.67e-11 //  N (Mm/kg)2;
 //const M = 5.972e24; // earth mass
 
@@ -10,8 +10,8 @@ const M = 5.972e14; // a mass
 
 
 class Point {
-    constructor(public position: Vector, public velocity: Vector = new Vector(0, 0)) { }
-    static new(position: Vector) { return new Point(position, new Vector(0, 0)); }
+    constructor(public position: Vector, public velocity: Vector = new Vector(0, 0), public halfAcceleration: Vector = new Vector(0, 0)) { }
+    static new(position: Vector) { return new Point(position, new Vector(0, 0), new Vector(0, 0)); }
 }
 
 class Vector {
@@ -85,24 +85,31 @@ class World {
         return diff.divIp(distance * distance * distance).mulIp(G_CST * M * M);
     }
 
-    update() {
+    update(timestep) {
         // update the position vector
         for (let p of this.planets) {
-            p.position.addIp(p.velocity);
+            p.position.addIp(
+                // (velocity + acceleration * timestep / 2) * timestep
+                p.velocity.add(p.halfAcceleration.mul(timestep)).mul(timestep)
+            );
         }
 
         // Update the velocity vector
         for (let p1 of this.planets) {
-            let sum_pull = new Vector(0, 0);
+            let sum_forces = new Vector(0, 0);
             for (let p2 of this.planets) {
-                // use the velocity Verlet method instead: http://gamedev.stackexchange.com/questions/15708/how-can-i-implement-gravity
+                // use the velocity Verlet method: http://gamedev.stackexchange.com/questions/15708/how-can-i-implement-gravity
                 if (!p1.position.equal(p2.position)) {
                     // pull_vector = direction_vector * m1*m2*G / (distance * distance)
-                    sum_pull.addIp(this.compute_force(p1, p2));
+                    sum_forces.addIp(this.compute_force(p1, p2));
                 }
             }
-            let acceleration = sum_pull.div(M);
-            p1.velocity.addIp(acceleration);
+            let newhalfAcceleration = sum_forces.div(2 * M);
+            p1.velocity.addIp(
+                // timestep * (accelerationv + newAcceleration) / 2
+                p1.halfAcceleration.add(newhalfAcceleration).mul(timestep)
+            );
+            p1.halfAcceleration = newhalfAcceleration;
         }
     }
 }
@@ -151,10 +158,10 @@ let ctx: CanvasRenderingContext2D = $canv.getContext("2d");
 let world = new World(WIDTH, HEIGHT, NB_PLANETS);
 let drawingBoard = new DrawingBoard(world, ctx);
 
-let counter = 0
+let time = 0
 setInterval(() => {
-    world.update();
+    world.update(TIMESTEP);
     drawingBoard.draw();
-    $message.innerHTML = "Frame: " + counter;
-    counter++;
+    $message.textContent = "time: " + time + "s.";
+    time += TIMESTEP;
 }, 500);
