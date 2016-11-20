02fe7ca44619a153641b1870b692fdd98cca3a90
Blizarre <github@marache.net>
2016-11-20 19:47:00 +0000

    extracted Drawing board from main script file

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,5 @@
 export const G_CST = 6.67e-11 //  N.(m/kg)2;
 export const DENSITY = 900000000; // kg / m^3
 export const DEFAULT_RESOLUTION = 40000; // m / pixel
+export const WIDTH = 1000;
+export const HEIGHT = 600;
--- src/phys.ts
+++ src/phys.ts
@@ -1,12 +1,11 @@
-let WIDTH = 1000;
-let HEIGHT = 600;
 const TIMESTEP = 0.5
 const NB_PLANETS = 10;
 
 
-import { G_CST, DEFAULT_RESOLUTION } from "./constants";
+import { G_CST, DEFAULT_RESOLUTION, WIDTH, HEIGHT } from "./constants";
 import Vector from "./Vector";
 import Planet from "./Planet";
+import DrawingBoard from "./DrawingBoard";
 
 
 class World {
@@ -86,76 +85,6 @@ class World {
         }
     }
 }
-
-
-class DrawingBoard {
-
-    offset: Vector;
-    resolution: number;
-
-    constructor(public ctx: CanvasRenderingContext2D) {
-        this.resolution = DEFAULT_RESOLUTION;
-        this.offset = new Vector(- WIDTH * this.resolution / 2, - HEIGHT * this.resolution / 2)
-    }
-
-    scale(factor) {
-        this.resolution *= factor;
-        this.offset = new Vector(- WIDTH * this.resolution / 2, - HEIGHT * this.resolution / 2)
-    }
-
-    draw(planets: Planet[], debug: boolean, renderTime: number) {
-        let ct = this.ctx;
-        ct.fillStyle = "black";
-        ct.fillRect(0, 0, ct.canvas.clientWidth, ct.canvas.clientHeight);
-        for (let p of planets) {
-            let relativePosition = p.position.sub(this.offset).divIp(this.resolution);
-
-            // Draw PLanet
-            ct.beginPath();
-            ct.fillStyle = p.color;
-            ct.arc(relativePosition.x, relativePosition.y, p.radius / this.resolution, 0, Math.PI * 2, true);
-            ct.fill();
-
-            if (debug) {
-                // Draw cross on each planet
-                ct.beginPath();
-                ct.strokeStyle = "white";
-                ct.lineWidth = 1;
-                ct.moveTo(relativePosition.x , relativePosition.y - 2);
-                ct.lineTo(relativePosition.x , relativePosition.y + 2);
-                ct.moveTo(relativePosition.x + 2, relativePosition.y);
-                ct.lineTo(relativePosition.x - 2, relativePosition.y);
-                ct.stroke();
-
-                // Draw Velocity vector: 2 pixel wide so that it can still be seen
-                // behind the acceleration
-                ct.beginPath();
-                ct.strokeStyle = "blue";
-                ct.lineWidth = 3;
-                ct.moveTo(relativePosition.x, relativePosition.y);
-                ct.lineTo(relativePosition.x + p.velocity.x / this.resolution, relativePosition.y + p.velocity.y / this.resolution);
-                ct.stroke();
-
-                // Draw aceleration vector: * 10 factor as it is nearly invisible
-                ct.beginPath();
-                ct.strokeStyle = "yellow";
-                ct.lineWidth = 1;
-                ct.moveTo(relativePosition.x, relativePosition.y);
-                ct.lineTo(relativePosition.x + 10 * p.acceleration.x / this.resolution, relativePosition.y + 10 * p.acceleration.y / this.resolution);
-                ct.stroke();
-            }
-        }
-        if(debug) {
-            // Draw render time for the previous frame
-            ct.fillStyle = "white";
-            ct.font = "16px sans-serif";
-            ct.fillText("render: " + renderTime + "ms.", 20, 20);
-            ct.fillText("[d] to disable", 20, 40);
-        }
-    }
-}
-
-
 
 let $canv = document.createElement("canvas");
 $canv.width = WIDTH;
