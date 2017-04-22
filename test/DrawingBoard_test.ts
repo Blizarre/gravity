54109f3097294291781532329b97b89c2910c6a0
Simon M <github@marache.net>
2017-04-22 18:57:16 +0100

    Added test to check move function

--- test/DrawingBoard_test.ts
+++ test/DrawingBoard_test.ts
@@ -5,8 +5,36 @@ import Vector from "../src/Vector";
 describe("Drawing Board", function() {
     let draw = new DrawingBoard(undefined);
 
-      it("will give the same pixel location after being transformed to world space and back", function() {
-          for(var i =-1000; i < 1000; i+=100)
+    it("can be moved across the world view", function() {
+        for(var i =-1000; i < 1000; i+=500)
+        {
+            for(var j =-1000; j < 1000; j+=500)
+            {
+                let center = new Vector(i, j)
+                console.log("(" + i + "," + j + ")")
+                // setcenter will change the central value in the WORLD coordinate system.
+                draw.setCenter(center)
+                // X--------------+
+                // |topLeftCorner | H
+                // |              | E
+                // |      X center| I
+                // |         (i,j)| G
+                // |              | H
+                // +----WIDTH-----+ T
+                expect(draw.topLeftCorner()).toEqual(
+                    draw.screenToWorld(new Vector(0, 0)))
+                expect(draw.worldToScreen(center)).toEqual(
+                    draw.getSize_px().div(2))
+            }
+        }
+    })
+
+    draw = new DrawingBoard(undefined);
+
+    it("will give the same pixel location after being transformed to world space and back", function() {
+      for(var i =-1000; i < 1000; i+=100)
+      {
+          for(var j =-1000; j < 1000; j+=100)
           {
               for(var scaling = 0.01; scaling < 1000; scaling *= 10) {
                   draw.move_px(i, j)
